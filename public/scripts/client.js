dca24ce943509ca4a3930a7608e7318a17e5accf
Your Name <parth.mungra94@gmail.com>
2022-01-05 00:08:15 +0000

    Cleaning code and fixed escape function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,46 +1,13 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
 $(document).ready(function() {
-
-  const escape =  function(str) {
-    let div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.exampleTweet;
-  }
-/*const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]*/
+  const escape = str => {
+      const div = document.createElement('div');
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
 
 const renderTweets = function(tweets) {
   $('#tweets-container').empty();
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
   for (const tweet of tweets) {
     const newTweet = createTweetElement(tweet)
     $('#tweets-container').prepend(newTweet);
@@ -72,11 +39,6 @@ $('.input').on('submit', function(event) {
 
 })
 
-/*$.ajax('/tweets', {
-  data: $(this).serialize(),
-  method: 'POST'
-});*/
-
 const loadTweets = function () {
   $.ajax('/tweets', {
     method: 'GET',
@@ -87,11 +49,9 @@ const loadTweets = function () {
 loadTweets();
 
 const createTweetElement = function(tweet) {
-  // const $tweet = $(`<article class="tweet">Hello world</article>`);
-    //create HTML
     const exampleTweet = `<article class="tweet">
           <header>
-            <img src= ${tweet.user.avatars}/>
+          <img src= ${escape(tweet.user.avatars)}/>
             <h4>${escape(tweet.user.name)}</h4>
             <p>${escape(tweet.user.handle)}</p>
           </header>
@@ -105,5 +65,4 @@ const createTweetElement = function(tweet) {
   return exampleTweet;
 };
 
-//renderTweets(data);
 });
