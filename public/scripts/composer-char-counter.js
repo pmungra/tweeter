704e83436c39195a45db38cdf51618a642168301
Your Name <parth.mungra94@gmail.com>
2022-01-07 08:31:04 +0000

    Added logic for red counter for cahr over 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -9,6 +9,14 @@ const $charCounter = $(this).siblings('.counter');
 //added variable to limit cahracter to 140
 const remainingCharCount = 140 - $(this).val().length;
 
+//turns char counter to red if goes above 140 cahracter 
+//https://dev.to/websolutionstuff/character-count-in-textarea-48p3 
+$charCounter.text(remainingCharCount);
+  if (remainingCharCount < 0) {
+    $charCounter.addClass('over-char-limit');
+    // Adds the class to each element in the set of matched elements.
+  } else {
+    $charCounter.removeClass('over-char-limit');
+  }  
 });
-
 });
